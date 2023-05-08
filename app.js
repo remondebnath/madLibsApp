729301f099105a524358cb1e2dc839faf113e876
Remon <remondebnath88@gmail.com>
2023-05-08 11:42:43 +0600

    changeit

---
+++ app.js
@@ -0,0 +1,5 @@
+let userInputs=[];
+
+let originalStory=`Thornton Gombar lifted his gaze to the sky as a hovercraft zoomed over his home. Based on the rosy pink tints that adorned its exterior, Thornton surmised that the vehicle was a pleasure craft, likely filled with a small group of men and women engaging in gross debauchery as the self-driving vehicle propelled itself across the sky. The craft jerked and jolted in an arrhythmic manner as it flew to the heart of Homasoro City, where even more such activities awaited them. `;
+console.log(originalStory);
+
